e5001fafabc4eb3b0c00ca5b0ee138bb8fdeaf2b
Pooja Tanganiya <pooja.k.tanganiya@apisero.com>
2024-01-08 00:18:43 +0530

    Initial commit for quetio microservice

---
+++ src/main/java/com/question/controller/QuestionController.java
@@ -0,0 +1,39 @@
+package com.question.controller;
+
+import com.question.entities.Question;
+import com.question.services.QuestionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping
+    public Question create(@RequestBody Question question){
+        return questionService.create(question);
+    }
+
+    @GetMapping
+    public List<Question> getAllQuestions(){
+        return questionService.getAll();
+    }
+
+    @GetMapping("/{questionId}")
+    public Question getQuestion(@PathVariable Long questionId){
+        return questionService.get(questionId);
+    }
+
+    @GetMapping("/quiz/{quizId}")
+    public List<Question> getQuestionsOfQuiz(@PathVariable Long quizId){
+        return questionService.getQuestionsOfQuiz(quizId);
+    }
+
+}
